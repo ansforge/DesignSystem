c6b4248fc1068f55d57f23ff85619920ed5793fe
mfaurel <michael.faurel@esante.gouv.fr>
2024-04-29 16:27:43 +0200

    feat: ajout du ds 1.0.9 dans le site part 2

--- docs/php/confirmation.php
+++ docs/php/confirmation.php
@@ -315,6 +315,10 @@
                 </li>
 
                 <li class="nav-item common-nav-item">
+                  <a class="nav-link" href="sg-support.html">Support</a>
+                </li>
+
+                <li class="nav-item common-nav-item">
                   <a class="nav-link" href="sg-versions.html">Versions</a>
                 </li>
               </ul>
